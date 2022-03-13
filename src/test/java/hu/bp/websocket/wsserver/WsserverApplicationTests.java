523105c9bad92e4bc02899e7744b716232a8f4b8
Peter Borkuti <peterborkuti@gmail.com>
2022-03-13 08:30:28 +0100

    springi initialzr - lombok + websocket

---
+++ src/test/java/hu/bp/websocket/wsserver/WsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bp.websocket.wsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
