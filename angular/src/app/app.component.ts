a7bb4f891067bc0433d0fc53859dadba7fa37896
Peter Borkuti <peterborkuti@gmail.com>
2022-03-13 20:31:47 +0100

    angular websocket code

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { map, Subject, takeUntil } from 'rxjs';
+import { RxStompService } from './rx-stomp.service';
+import { Message } from '@stomp/stompjs';
+
+type DemoMessage = {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy{
   title = 'angular';
+  receivedMessages: string[] = [];
+  textInput = "";
+
+  private unsubscribe$ = new Subject<boolean>();
+
+  constructor(private rxStompService: RxStompService) { }
+
+  ngAfterViewInit(): void {
+    this.rxStompService.watch('/topics/messagesTopic')
+    .pipe(
+      takeUntil(this.unsubscribe$),
+      map((msg: Message) => JSON.parse(msg.body) as DemoMessage)
+    )
+    .subscribe((message: DemoMessage) => {
+      this.receivedMessages.push(message.message);
+    });
+  }
+
+  onSendMessage() {
+    const message: DemoMessage = { message: this.textInput };
+    this.rxStompService.publish({ destination: '/messages/echo', body: JSON.stringify(message) });
+  }
+  
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
+  }
+
+ 
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { rxStompServiceFactory } from './rx-stomp-service-factory';
+import { RxStompService } from './rx-stomp.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
